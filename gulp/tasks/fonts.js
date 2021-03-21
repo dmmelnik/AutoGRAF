e76ac2696390a5cc3f0ed369b1ae56b5bfafa6a1
Melnik Dmitry <dmmelniks@gmail.com>
2021-03-21 21:30:38 +0300

    [+] Фонты [+] SVG [+] основные цвета [+] Шапка в десктопе

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp')
 
 module.exports = function fonts() {
-  return gulp.src('src/fonts/*')
+  return gulp.src(['src/fonts/**', '!src/fonts/stylesheet.css'])
     .pipe(gulp.dest('build/fonts'))
 }
 
